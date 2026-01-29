a31bfd4699fa6c0725c469aa27a99b6e76c78fc2
Lucas Dewil <lucas.dewil@student.howest.be>
2026-01-29 16:28:56 +0100

    Connector scripting v2

---
+++ SterdriehoekSchakelingVR/Assets/Scripts/TriggerScripts/ConnectorScript.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class ConnectorScript : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer _meshRenderer;
+    [SerializeField] private Material _materialFault;
+    [SerializeField] private string _NeededName = "connector";
+
+    [SerializeField] private GameObject _Bolt;
+    // This script checks if the player has entered the fuse box area
+    private bool _IsTriggerd = false;
+    public bool IsTriggerd {
+        get { return _IsTriggerd; }
+        set
+        {
+            if (value != _IsTriggerd )
+            {
+                _IsTriggerd = value;
+                if (_IsTriggerd) Debug.Log("CableTriggerScript: Triggered");
+            }
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(_NeededName))
+        {
+            _IsTriggerd = true;
+            Debug.Log("CableTriggerScript: Triggered" + _NeededName);
+            // Get the XR Grab Interactable component
+            other.GetComponent<XRGrabInteractable>().enabled = false;
+            // Disable the mesh 
+            if (_meshRenderer !=null)
+            {
+                _meshRenderer.enabled = false;
+            }
+
+            if (_Bolt != null)
+            {
+                _Bolt.SetActive(true);    
+            }
+            
+        }
+        else
+        {
+            if (_meshRenderer != null)
+            {
+                _meshRenderer.material = _materialFault;    
+            }
+        }
+    }
+}
