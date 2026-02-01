95fe483dedcd79e5c43ee05fa59162cd35ff27fa
Lucas Dewil <lucas.dewil@student.howest.be>
2026-02-01 13:06:25 +0100

    Fixed bug grab

---
+++ SterdriehoekSchakelingVR/Assets/Scripts/TriggerScripts/LidTriggerScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class LidTriggerScript : MonoBehaviour
+{
+    // This script checks if the player has entered the fuse box area
+    private bool _IsTriggerd = false;
+    public bool IsTriggerd {
+        get { return _IsTriggerd; }
+        set
+        {
+            if (value != _IsTriggerd )
+            {
+                _IsTriggerd = value;
+                if (_IsTriggerd) Debug.Log("CableTriggerScript: Triggered");
+            }
+        }
+    }
+    
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Lid"))
+        {
+            IsTriggerd = true;
+        }
+    }
+    
+    
+}
